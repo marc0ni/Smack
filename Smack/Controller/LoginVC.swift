90cdc30fecd9f0e86d4cdd2fed3980ad9c9a5982
Mark Lindamood <mark.lindamood@yahoo.com>
2017-10-06 19:35:34 -0700

    Added LoginVC UI

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Mark Lindamood on 10/6/17.
+//  Copyright © 2017 udemy. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Mark Lindamood on 10/6/17.
+//  Copyright © 2017 udemy. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
