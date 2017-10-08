542ab4bea5332496d2cdd983d3a1f8a0acd1852b
Mark Lindamood <mark.lindamood@yahoo.com>
2017-10-07 21:15:09 -0700

    Added outlets and actions for CreateAccountVC UI items; added "register user" func to CreateAccountVC

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
-
+    
+    //Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,5 +26,20 @@ class CreateAccountVC: UIViewController {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
     
-
+    @IBAction func createAcctPressed(_ sender: Any) {
+        guard let email = emailTxt.text, emailTxt.text != " " else {return}
+        guard let password = passwordTxt.text, passwordTxt.text != " " else {return}
+        
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("registered user!")
+            }
+        }
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+    }
 }
