88965391215b3c48de81ddac6801ef7e93de430a
Mark Lindamood <mark.lindamood@yahoo.com>
2017-10-09 21:51:37 -0700

    Added background colors for avatar image

---
+++ Smack/View/CircleImage.swift
@@ -0,0 +1,28 @@
+//
+//  CircleImage.swift
+//  Smack
+//
+//  Created by Mark Lindamood on 10/9/17.
+//  Copyright © 2017 udemy. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleImage: UIImageView {
+
+    override func awakeFromNib() {
+        setUpView()
+    }
+    
+    func setUpView() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setUpView()
+    }
+
+}
