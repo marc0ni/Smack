d6cf02ad4ec75671babe1033663eb36f7f4a2662
Mark Lindamood <mark.lindamood@yahoo.com>
2017-10-10 19:04:25 -0700

    Set-up ProfileVC UI

---
+++ Smack/Controller/ProfileVC.swift
@@ -0,0 +1,31 @@
+//
+//  ProfileVC.swift
+//  Smack
+//
+//  Created by Mark Lindamood on 10/10/17.
+//  Copyright © 2017 udemy. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userEmail: UILabel!
+    @IBOutlet weak var bgView: UIView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func closeModalPressed(_ sender: Any) {
+    }
+    @IBAction func logoutPressed(_ sender: Any) {
+    }
+    
+}
